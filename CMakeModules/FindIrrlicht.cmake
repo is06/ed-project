4989a4afed398ae6a1188c622f9b3ac5bd6fca57
Metallizer <thomas.noury@protonmail.com>
2021-04-08 20:12:29 +0200

    Init

---
+++ CMakeModules/FindIrrlicht.cmake
@@ -0,0 +1,27 @@
+find_path(
+    IRRLICHT_INCLUDE_DIR
+        NAMES
+            irrlicht.h
+        PATH_SUFFIXES
+            irr
+            irrlicht
+)
+
+find_library(
+    IRRLICHT_LIBRARY
+        NAMES
+            Irrlicht
+        PATH_SUFFIXES
+            irr
+            irrlicht
+)
+
+include(FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args(
+    Irrlicht
+    IRRLICHT_INCLUDE_DIR
+    IRRLICHT_LIBRARY
+)
+
+mark_as_advanced(IRRLICHT_INCLUDE_DIR IRRLICHT_LIBRARY)
